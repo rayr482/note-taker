993de37a9b501922e54f69d8009d226a5fb68896
rayr482 <rar002@ucsd.edu>
2023-06-20 11:12:02 -0700

    had issues with routes, so placed everything into server.js

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const path = require('path');
+const fs = require('fs');
 
 const app = express();
 const PORT = 3001;
@@ -7,8 +9,31 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require('./routes/api')(app);
-require('./routes/html')(app);
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './db/db.json'));
+});
+
+app.post('/api/notes', (req, res) => {
+    let db = JSON.parse(fs.readFileSync('db/db.json'));
+    res.json(db);
+
+    let note = {
+        title: req.body.title,
+        text: req.body.text
+    };
+
+    db.push(note);
+    fs.writeFileSync('db/db.json', JSON.stringify(db));
+    res.json(db);
+});
 
 app.listen(PORT, () => {
     console.log(`App listening at http://localhost:${PORT}`);
